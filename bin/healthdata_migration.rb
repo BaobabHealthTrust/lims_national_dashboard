d29d67769a790a17800148ec6046169ee38f491a
phundi <kennethkapundi1@gmail.com>
2018-03-28 14:03:19 +0200

    Updated script to check last tracking #

--- bin/healthdata_migration.rb
+++ bin/healthdata_migration.rb
@@ -1,7 +1,7 @@
 load "bin/tracking_number_service.rb"
-settings = YAML.load_file("#{Rails.root}/config/application.yml")
-configs = YAML.load_file("#{Rails.root}/config/couchdb.yml")[Rails.env]
-
+$settings = YAML.load_file("#{Rails.root}/config/application.yml")
+$configs = YAML.load_file("#{Rails.root}/config/couchdb.yml")[Rails.env]
+$counter_hash = JSON.parse(File.read("#{Rails.root}/public/tracker.json")) rescue {}
 puts "Enter MYSQL HOST e.g 0.0.0.0"
 host = gets.chomp
 
@@ -18,9 +18,9 @@ puts "Enter ART database name"
 art_db = gets.chomp
 
 puts "Enter National LIMS Couch Database"
-lims_db = gets.chomp
-
-puts "ART Database: #{art_db} , HealthData database: #{healthdata_db},  SQL Username: #{user}, LIMS database: #{lims_db}"
+$lims_db = gets.chomp
+$couch_query_url = "#{$configs['protocol']}://#{$configs['username']}:#{$configs['password']}@#{$configs['host']}:#{$configs['port']}/#{$lims_db}/_design/Order/_view/generic"
+puts "ART Database: #{art_db} , HealthData database: #{healthdata_db},  SQL Username: #{user}, LIMS database: #{$lims_db}"
 
 puts "Continue migration using details above? (y/n)"
 proceed = gets.chomp
@@ -29,6 +29,13 @@ if proceed.downcase.strip != 'y'
   puts "Migration Stopped"
   Process.kill 9, Process.pid
 end
+
+puts "Initialising generic view in NLIMS database" 
+Order.generic.count
+puts "Finished initialisation"
+
+File.open("#{Rails.root}/public/tracker.json", 'w') {|f|
+      f.write({}.to_json) }
 
 con = Mysql2::Client.new(:host => host,
                          :username => user,
@@ -47,7 +54,39 @@ bulk = {
     "docs" => []
 }
 
+def check_counter_per_date(date)
+ date = date.strftime("%Y%m%d")
+ if $counter_hash[date].blank? 
+  do_reset(date)
+ end 
+end  
+
+def do_reset(date)
+  start_date = date.to_date.strftime("%Y%m%d000000")
+  end_date = date.to_date.strftime("%Y%m%d235959")
+
+  query_link = $couch_query_url + "?startkey=[\"#{start_date}\"]&endkey=[\"#{end_date}\"]"
+  available_samples = JSON.parse(RestClient.get(URI.encode(query_link), :content_type => "application/json"))
+  tracking_numbers = available_samples['rows'].collect{|h| h['id']}
+  max_track_num    = tracking_numbers.max.to_s.strip
+  puts max_track_num 
+  puts date
+  daily_counter = max_track_num.reverse[0 .. 2].reverse.to_i 
+ 
+  if daily_counter >= 0
+    file = JSON.parse(File.read("#{Rails.root}/public/tracker.json")) rescue {}
+    todate = date.to_date.strftime("%Y%m%d")
+
+    file[todate] = daily_counter + 1
+    File.open("#{Rails.root}/public/tracker.json", 'w') {|f|
+
+      f.write(file.to_json) }
+  end
+end 
+
 samples.each_with_index do |row, i|
+  check_counter_per_date(row['OrderDate'].to_date)
+
   puts "#{(i + 1)}/#{total}"
   patient = bart2_con.query(
                 "SELECT n.given_name, n.middle_name, n.family_name, p.birthdate, p.gender, pid2.identifier npid, sd.earliest_start_date start_date
@@ -108,12 +147,12 @@ h["TestOrdered"]}
       "date_dispatched"=> "",
       "art_start_date"=> (patient['start_date'].to_datetime.strftime("%Y%m%d%H%M%S") rescue nil),
       "date_received"=>  (row['RcvdAtLabDate'].blank? ? nil : "#{row['RcvdAtLabDate'].to_date.strftime('%Y%m%d')}000000"),
-      "sending_facility"=> settings['site_name'],
-      "receiving_facility"=> settings['target_lab'],
+      "sending_facility"=> $settings['site_name'],
+      "receiving_facility"=> $settings['target_lab'],
       "reason_for_test"=> "",
       "test_types"=> tests,
       "status"=> "Drawn",
-      "district"=> settings['district'],
+      "district"=> $settings['district'],
       "priority"=> "Routine",
       "order_location"=> row['Location'],
       "results"=> formatted_results,
@@ -122,6 +161,6 @@ h["TestOrdered"]}
 end
 
 if bulk['docs'].length  > 0
-  url = "#{configs['protocol']}://#{configs['username']}:#{configs['password']}@#{configs['host']}:#{configs['port']}/#{lims_db}/_bulk_docs"
+  url = "#{$configs['protocol']}://#{$configs['username']}:#{$configs['password']}@#{$configs['host']}:#{$configs['port']}/#{$lims_db}/_bulk_docs"
   RestClient.post(url, bulk.to_json, :content_type => "application/json")
 end
